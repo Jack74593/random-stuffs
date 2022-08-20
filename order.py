940d628b45d4acf87651a3b7e800c2b4ce828436
Jack74593 <76830382+Jack74593@users.noreply.github.com>
2022-08-20 22:10:05 +0700

    a thing for my school project
    its in vietnamese so i dont think you will understand the stuff i put in lmao

---
+++ order.py
@@ -0,0 +1,51 @@
+#Code đa phần được skid từ https://replit.com/@ThyNguyn20/FastRoyalblueProtools :P
+
+import string
+import random
+
+a = 1
+b = 2
+c = 10
+d = ("1 cốc")
+
+print("*"*30)
+print("Phần mềm order đồ ăn quán :3 ")
+print("*"*30)
+
+print("Menu của Quán: ")
+print("=============================")
+print("1 - Burger thịt bò phô mai 25k")
+print("2 - Burger thịt gà 25k")
+print("3 - Đùi gà rán (2 cái) - 30k")
+print("4 - Gà không xương (10 miếng) - 30k")
+print("5 - Khoai tây chiên - 20k")
+print("=============================")
+print("6 - Coke/Fanta/Sprite (450ml) - 25k")
+print("7 - Trà chanh (250ml) - 15k")
+print("8 - Kem Vanilla/Chocolate - 10k")
+print("9 - Bánh Brownies - 12k")
+print("10 - Bánh Kem - 12k")
+choice = input('Hãy nhập số của món bạn muốn ăn: ')
+
+if choice == '1':
+  print("Bạn đã chọn",a,"Burger thịt bò phô mai.")
+if choice == '2':
+  print('Bạn đã chọn',a,'Burger thịt gà.')
+if choice == '3':
+  print('Bạn đã chọn',b,'hai miếng đùi gà rán.')
+if choice == '4':
+  print('Bạn đã chọn',c,'miếng gà không xương.')
+if choice == '5':
+  print('Bạn đã chọn',a,'suất khoai tây chiên.')
+if choice == '6':
+  print('Bạn đã chọn',d,'đồ uống.')
+if choice == '7':
+  print('Bạn đã chọn',d,'Trà chanh.')
+if choice == '8':
+  print('Bạn đã chọn',a,'cây kem.')
+if choice == '9':
+  print('Bạn đã chọn',a,'miếng bánh Brownies.')
+if choice == '10':
+  print('Bạn đã chọn',a,'miếng bánh kem.')
+
+
